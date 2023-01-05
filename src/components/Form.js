fa8446fc972caa4392d51600e1ac5732eae7e841
Rhibro <111956763+Rhibro@users.noreply.github.com>
2023-01-05 10:27:28 +0100

    working on form

--- src/components/Form.js
+++ src/components/Form.js
@@ -15,39 +15,28 @@ function Form() {
         }
     };
 
+    const submitHandler = (event) => {
+        event("Successfully submitted!");
+    };
+
     return (
         <div className="formPage">
             <h1>Contact Me</h1>
-            <form className="myForm">
+            <form onSubmit={submitHandler} className="myForm">
                 <label htmlFor="name">Your Name</label>
-                <input type="text" id="name" required></input>
+                <input type="text" name="name" required></input>
+
                 <label htmlFor="email">Your Email</label>
-                <input type="text" name="name" value={email} onChange={handleInput} required />
+                <input type="text" name="email" value={email} onChange={handleInput} required />
                 <p style={{ color: "red"}}>{error && error}</p>
+                
                 <label htmlFor="message">Your Message</label>
                 <textarea name="message" id="message" class="message-box" cols="30" rows="5" required></textarea>
-                <button type="submit">Submit</button>
+               
+                <button type="submit" className="formBtn">Submit</button>
             </form>
         </div>
     )
 }
-
-
-/*const Form = () => {
-    return (
-        <div className="formPage">
-            <h1>Contact Me</h1>
-            <form className="myForm">
-                <label htmlFor="name">Your Name</label>
-                <input type="text" id="name" required></input>
-                <label htmlFor="email">Your Email</label>
-                <input type="email" id="email" required></input>
-                <label htmlFor="message">Your Message</label>
-                <textarea name="message" id="message" class="message-box" cols="30" rows="10" required></textarea>
-                <button type="submit">Submit</button>
-            </form>
-        </div>
-    );
-}*/
 
 export default Form;
