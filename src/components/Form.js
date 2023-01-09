74daf96ec4658b38a6d163fffc21c36f04f5ad86
Rhibro <111956763+Rhibro@users.noreply.github.com>
2023-01-09 17:11:18 +0100

    updated form

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,21 +2,40 @@ import React from "react";
 import { useState } from "react";
 
 function Form() {
-    const [email, setEmail] = useState("");
+    const [person, setPerson] = useState({
+        fullName: "",
+        email: "",
+        message: "",
+    });
+    const [message, setMessage] = useState("");
     const [error, setError] = useState("");
 
     const handleInput = (e) => {
+        const fullName = e.target.fullName;
         const value = e.target.value;
-        setEmail(e.target.value);
-        if (email !== ""(value)) {
-            setError("Invaild Email");
+
+        setPerson({...person, [fullName]: value});
+
+    };
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        
+        if (person.fullName && person.email && person.message === "") {
+            setError("All fields are required!");
         } else {
             setError("");
         }
-    };
+        console.log(person);
 
-    const submitHandler = (event) => {
-        event(alert("Successfully submitted!"));
+        const newMessage = {...person.message};
+        setMessage(...message, newMessage);
+
+        setPerson({
+            fullName: "",
+            email: "",
+            message: "",
+        })
     };
 
     return (
@@ -24,16 +43,49 @@ function Form() {
             <h1>Contact Me</h1>
             <form onSubmit={submitHandler} className="myForm">
                 <label htmlFor="name">Your Name</label>
-                <input type="text" name="name" required></input>
+                <input 
+                    type="text" 
+                    name="name" 
+                    value={person.fullName}
+                    onChange={handleInput}
+                    required>
+                </input>
 
                 <label htmlFor="email">Your Email</label>
-                <input type="text" name="email" value={email} onChange={handleInput} required />
+                <input 
+                    type="text" 
+                    name="email" 
+                    value={person.email} 
+                    onChange={handleInput} 
+                    required 
+                />
                 <p style={{ color: "red"}}>{error && error}</p>
                 
                 <label htmlFor="message">Your Message</label>
-                <textarea name="message" id="message" class="message-box" cols="30" rows="5" required></textarea>
+                <textarea 
+                name="message" 
+                id="message" 
+                className="message-box" 
+                cols="30" 
+                rows="5" 
+                value={person.message}
+                onChange={handleInput}
+                required>
+                </textarea>
                
-                <button type="submit" className="formBtn">Submit</button>
+                <button type="submit" className="formBtn">
+                    Submit
+                </button>
+               {message.localeCompare((msg) => {
+                const {fullName, email, message} = msg;
+                return (
+                    <div>
+                        <h4>{fullName}</h4>
+                        <p>{email}</p>
+                        <p>{message}</p>
+                    </div>
+                );
+               })}
             </form>
         </div>
     )
