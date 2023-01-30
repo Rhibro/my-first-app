1c47f281b3df25fbda2374163fcafd56a6a08405
Rhibro <111956763+Rhibro@users.noreply.github.com>
2023-01-30 16:59:28 +0100

    changed background

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
-import Header from "./components/Header";
+//import Header from "./components/Header";
 import About from "./components/About";
-import Footer from "./components/Footer";
+//import Footer from "./components/Footer";
 import Contact from "./components/Contact";
 
 function App() {
   return (
     <BrowserRouter>
-    <div>
-      <Header />
-    </div>
+    
     <div className="container">
       <Routes>
         <Route path="/" element={<Home />} />
@@ -18,9 +16,7 @@ function App() {
         <Route path="/contact" exact element={<Contact />} />
       </Routes>
     </div>
-    <div>
-      <Footer />
-    </div>
+   
     </BrowserRouter>
   );
 }
