8826db6ad446b19aeee3a6931c172646c0296167
Rhibro <111956763+Rhibro@users.noreply.github.com>
2023-03-01 11:12:55 +0100

    added content to about me

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,22 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import Button from "./Button";
+//import Button from "./Button";
+import AboutImg from "./AboutImg";
+import circle from "../images/circle.png"
 
 const About = () => {
     return (
-        <div>
-            <h1>About Me</h1>
-            <Button>HELLO</Button>
+        <div className="aboutBody">
+            <div className="aboutTitleBox">
+                <AboutImg />
+                <h1 className="aboutMe">about me</h1>
+            </div>
+            <div className="aboutInfoBox">
+                <img className="myFace" src={circle}></img>
+                <p className="aboutInfo">
+                    Born and raised in Toronto, Canada I now call Malmö, Sweden home.
+                    If the future is tech then I want to make that future as accessible as possible for all.
+                </p>
+            </div>
         </div>
         );
     };
